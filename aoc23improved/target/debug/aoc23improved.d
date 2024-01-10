9b016d49248d2186c46566c91d2811ecd5e8bf23
Petros Minoudis <petros.minoudis@samedi.de>
2024-01-10 21:10:45 +0100

    Try optimizatng

--- aoc23improved/target/debug/aoc23improved.d
+++
@@ -1,1 +0,0 @@
-/Users/petros.minoudis/adventofcode2023/aoc23improved/target/debug/aoc23improved: /Users/petros.minoudis/adventofcode2023/aoc23improved/src/main.rs
--- aoc23improved/target/debug/deps/aoc23improved-3fad501d7744c630.d
+++
@@ -1,5 +0,0 @@
-/Users/petros.minoudis/adventofcode2023/aoc23improved/target/debug/deps/aoc23improved-3fad501d7744c630.rmeta: src/main.rs
-
-/Users/petros.minoudis/adventofcode2023/aoc23improved/target/debug/deps/aoc23improved-3fad501d7744c630.d: src/main.rs
-
-src/main.rs:
