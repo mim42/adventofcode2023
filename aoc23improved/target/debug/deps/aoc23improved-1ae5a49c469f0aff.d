64cfe427d4ef2410573638d7fa664f3ce329f7eb
Petros Minoudis <petros.minoudis@samedi.de>
2024-01-10 21:10:45 +0100

    improve day 23 part b

--- aoc23improved/target/debug/deps/aoc23improved-1ae5a49c469f0aff.d
+++
@@ -1,5 +0,0 @@
-/Users/petros.minoudis/adventofcode2023/aoc23improved/target/debug/deps/aoc23improved-1ae5a49c469f0aff.rmeta: src/main.rs
-
-/Users/petros.minoudis/adventofcode2023/aoc23improved/target/debug/deps/aoc23improved-1ae5a49c469f0aff.d: src/main.rs
-
-src/main.rs:
--- aoc23improved/target/debug/deps/aoc23improved-e3704db23f7e69ae.d
+++
@@ -1,5 +0,0 @@
-/Users/petros.minoudis/adventofcode2023/aoc23improved/target/debug/deps/aoc23improved-e3704db23f7e69ae: src/main.rs
-
-/Users/petros.minoudis/adventofcode2023/aoc23improved/target/debug/deps/aoc23improved-e3704db23f7e69ae.d: src/main.rs
-
-src/main.rs:
